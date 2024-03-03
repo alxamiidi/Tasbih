9586dc6e3a6e944c4a0d30e746084ec616d2f1b5
alxamiidi <jjpoi9416@gmail.com>
2024-03-03 14:42:53 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+
+function Header(){
+    return ( <div className='bg-teal-600 p-5'>
+      <h1 className="text-4xl text-center font-bold text-white">Tasbih Project</h1>
+    </div>
+    );
+  }
+export default Header  
